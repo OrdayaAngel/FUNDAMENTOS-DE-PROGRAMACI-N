fbaded03ee444c9cf2e98f159e9c04969a43d5f0
OrdayaAngel <V02210H@ms.upla.edu.pe>
2026-04-27 19:02:21 -0500

    SumaTresNumeros.java
    Suma De Tres Números

---
+++ SumaTresNumeros.java
@@ -0,0 +1,10 @@
+public class SumaTresNumeros {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int suma = a + b + c;
+        
+        System.out.println("La suma es: " + suma);
+    }
+}
